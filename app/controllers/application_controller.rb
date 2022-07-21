a4ad014c32894447facf66fa553080bc6d83f668
Tanvin <tanvin.sharma@toptal.com>
2022-07-21 21:23:12 +0530

    Co-authored-by: Makar Ermokhin <makar.ermokhin@toptal.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery with: :null_session
+
+  # before_action :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,32 @@
 class TweetsController < ApplicationController
-    def index
-        @tweets = Tweet.all
-        render "tweets/index"
-    end
+  before_action :authorize_user, only: [:create, :new]
+  def index
+    @tweets = Tweet.all
+    render "tweets/index"
+  end
+  
+  def new
     
-    def new
-
+  end
+  
+  def create
+    @tweet = @current_user.tweets.new(content: params[:content])
+    if @tweet.save
+      flash[:notice] = "tweet created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = @tweet.errors.full_messages
+      render 'new'
     end
-
-    def create
-        tweet = Tweet.create(content: params[:content])
-        puts(tweet.errors.full_messages)
-        redirect_to('/')
+  end
+  
+  private
+  
+  def authorize_user
+    unless logged_in?
+      flash.alert = "Please log in."
+      session[:redirected_from] = request.path
+      redirect_to login_url
     end
+  end
 end
