494cf09130d6ba1ed31af4e1ea87544b53c04879
Tanvin <tanvin.sharma@toptal.com>
2022-08-11 20:41:18 +0530

    async

---
+++ app/jobs/find_likes_job.rb
@@ -0,0 +1,19 @@
+require 'date'
+require 'csv'
+
+class FindLikesJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id, ts_from, ts_to)
+    user = User.find(user_id)
+    start_date, end_date = Date.parse(ts_from), Date.parse(ts_to)
+    # likes = Like.select { |like| like.tweet.user_id == user.id }
+    likes = Like.where(tweet:  user.tweets, created_at: (start_date..end_date))
+    CSV.open("./likes_report.csv", 'w+') do |add|
+      (start_date..end_date).each do |date|
+        like_count = likes.where(created_at: (date.beginning_of_day..date.end_of_day)).count
+        add << [date.strftime("%d/%m/%Y"), like_count] 
+      end
+    end
+  end
+end 
---
+++ spec/jobs/find_likes_job.rb
@@ -0,0 +1,24 @@
+require 'csv'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
+RSpec.describe 'Report Geneator' do
+  before do
+    Timecop.freeze("23/06/2022")
+
+    user = User.create(name: "tan", handle:"tan", bio: "tan", email: "tan@toptal.com", password:"123", password_confirmation: "123") 
+    tweet = user.tweets.create(content: "content")
+    like = tweet.likes.create(tweet_id: tweet.id, user_id: user.id)
+    FindLikesJob.perform_now(user.id, "23/06/2022", "25/06/2022") 
+  end
+
+  after { Timecop.return }
+
+  let!(:file_path) { "./likes_report.csv" }
+  let!(:csv) { CSV.open(file_path, "r") }
+  
+  specify { expect(csv.read).to eq([
+    ["23/06/2022", "1"],
+    ["24/06/2022", "0"], 
+    ["25/06/2022", "0"]]) }
+end
